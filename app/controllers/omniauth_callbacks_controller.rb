4af59f3f34afeafa14541f7fee14182bcbc50ce9
Mikhail Dubov <mdubov@gmail.com>
2014-06-16 19:01:20 +0100

    Added Linkedin authorization

--- app/controllers/omniauth_callbacks_controller.rb
+++ app/controllers/omniauth_callbacks_controller.rb
@@ -1,17 +1,12 @@
 class OmniauthCallbacksController < Devise::OmniauthCallbacksController
-
   def linkedin
-    @user = User.find_for_oauth(request.env["omniauth.auth"])
-    provider = @user.provider
-
+    auth = env["omniauth.auth"]
+    @user = User.connect_to_linkedin(request.env["omniauth.auth"],current_user)
     if @user.persisted?
-      sign_in @user
-      # sign_in_and_redirect @user, :event => :authentication
-      set_flash_message(:notice, :success, :kind => provider.capitalize) if is_navigational_format?
-      redirect_to charts_path
+      flash[:notice] = I18n.t "devise.omniauth_callbacks.success"
+      sign_in_and_redirect @user, :event => :authentication
     else
-      session["devise.#{provider}_data"] = request.env["omniauth.auth"]
+      session["devise.linkedin_uid"] = request.env["omniauth.auth"]
       redirect_to new_user_registration_url
     end
   end
-end
