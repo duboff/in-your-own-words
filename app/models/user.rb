300613e2508f32dab51fd8b6e484160165f34b0b
Mikhail Dubov <mdubov@gmail.com>
2014-06-19 00:15:37 +0100

    debugging

--- app/models/user.rb
+++ app/models/user.rb
@@ -21,8 +21,13 @@ class User < ActiveRecord::Base
         client = LinkedIn::Client.new(Rails.application.secrets[:linkedin_key], Rails.application.secrets[:linkedin_token],auth.access_token)
         puts '*' * 50
         p auth.to_h
-        client.profile(fields: %w(skills))
-        p client.skills.map {|skill| skill.name }
+        puts 'raw info'
+        p auth.raw_info
+        puts 'Extra'
+        p auth.extra
+        puts 'profile'
+        p client.profile
+        # p client.skills.map {|skill| skill.name }
         puts '*' * 50
         user = User.create(name:auth.info.name,
                            picture_url:auth.info.image,
