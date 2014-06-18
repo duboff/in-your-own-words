6eeaf29cf28628dbdbf5ec6d08382e76320bbbd5
Mikhail Dubov <mdubov@gmail.com>
2014-06-18 23:36:01 +0100

    debugging

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ActiveRecord::Base
 
   has_and_belongs_to_many :skills
-  # attr_accessor :client
+  attr_accessor :client
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
