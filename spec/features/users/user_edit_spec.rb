3ee87217896d3a9d0cff62b8cec098d6547ff6e8
Mikhail Dubov <mdubov@gmail.com>
2014-06-18 15:58:32 +0100

    User is redirected back to profile page on edit

--- spec/features/users/user_edit_spec.rb
+++ spec/features/users/user_edit_spec.rb
@@ -18,11 +18,10 @@ feature 'User edit', :devise do
   scenario 'user changes email address' do
     user = FactoryGirl.create(:user)
     login_as(user, :scope => :user)
-    visit edit_user_registration_path(user)
-    fill_in 'Email', :with => 'newemail@example.com'
-    fill_in 'Current password', :with => user.password
+    visit edit_user_path(user)
+    fill_in 'Name', :with => 'bla'
     click_button 'Update'
-    expect(page).to have_content 'You updated your account successfully.'
+    expect(page).to have_content 'bla'
   end
 
   # Scenario: User cannot edit another user's profile
