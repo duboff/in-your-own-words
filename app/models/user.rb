5c30c3d833d3f404629672a407214849a002e7f3
Mikhail Dubov <mdubov@gmail.com>
2014-06-18 23:06:23 +0100

    debugging

--- app/models/user.rb
+++ app/models/user.rb
@@ -21,7 +21,7 @@ class User < ActiveRecord::Base
         # self.client = LinkedIn::Client.new(Rails.application.secrets[:linkedin_key], Rails.application.secrets[:linkedin_token],auth.access_token)
         puts '*' * 50
         p auth.raw_info
-        p auth.raw_info.skills
+        p auth
         puts '*' * 50
         user = User.create(name:auth.info.name,
                            picture_url:auth.info.image,
